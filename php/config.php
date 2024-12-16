20a495cdf79fe2611b63e3e1dca66a6fd42472a5
alexcarsan <alexcarrero05@iesamachado.org>
2024-12-16 14:02:21 +0100

    16 dic

---
+++ php/config.php
@@ -0,0 +1,13 @@
+<?php
+
+    // Defino un par de constantes
+
+    define("NOMBRE", "Alex Carrero");
+    define("GRADO", "ASIR");
+
+    // Defino un par de variables
+
+    $asig = "IAW";
+    $anyo = "2";
+
+?>
